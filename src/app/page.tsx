d57ee329ab2c9ab2367737172a463c99e1d6a994
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-22 16:06:13 +0700

    Add: styles in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <p className="text-5xl font-bold">montserrat</p>
-      <h1 className="text-5xl font-mono">montserrat</h1>
-      <h1 className="text-5xl font-montserrat">montserrat</h1>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
     </div>
   );
 }
