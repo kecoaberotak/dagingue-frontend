991ba0501c8ab5c9594be05d2318949c939d7235
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-24 16:53:18 +0700

    Feat: fecth data from API using function from service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 import Header from "@/components/organism/Header";
 import { getMedia } from "@/services/content.service";
+import { MediaResponse } from "@/types/response.types";
 
-export default async function Home() {
-  getMedia((res, error) => {
-    if (error) {
-      console.error("Error fetching media:", error.message);
-    } else if (res) {
-      console.log("Media data:", res);
+export default async function Homepage() {
+  try {
+    // fetch data API
+    const data: MediaResponse = await getMedia();
+    console.log(data, "DATA");
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      // Error message berisi JSON yang dikirim dari service
+      throw new Error(error.message);
+    } else {
+      throw new Error("Unknown error occurred");
     }
-  });
-
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  }
 }
