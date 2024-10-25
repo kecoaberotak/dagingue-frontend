62ce28f4537dabeb952f9599952c2e3e42769610
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-25 22:10:22 +0700

    Create: interface types for header and socmed

---
+++ src/types/components.types.ts
@@ -0,0 +1,13 @@
+export interface HeaderMedia {
+  hero_image: string;
+  logo_image: string;
+  whatsapp: string;
+  instagram: string;
+  shopee: string;
+}
+
+export interface SocmedMedia {
+  whatsapp: string;
+  instagram: string;
+  shopee: string;
+}
