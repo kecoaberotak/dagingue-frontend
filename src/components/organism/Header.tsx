4ca9dcf516280ff701fdb70b04a84cd5eb28c6dc
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-25 22:08:54 +0700

    Add: add styles, logo, navbar, and socmed links

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -1,7 +1,30 @@
-const Header = () => {
+import Image from "next/image";
+import "@/components/organism/header.style.css";
+import { HeaderMedia } from "@/types/components.types";
+import Socmed from "../molecules/Socmed";
+
+interface HeaderProps {
+  headerMedia: HeaderMedia;
+}
+
+const Header: React.FC<HeaderProps> = ({ headerMedia }) => {
   return (
-    <header id="header">
-      <h1>Header</h1>
+    <header id="home" className="header" style={{ backgroundImage: `url(${headerMedia.hero_image})` }}>
+      <div className="logo">
+        <Image src={headerMedia.logo_image} width={500} height={500} alt="Logo Dagingue" />
+      </div>
+      <ul className="navbar-header">
+        <li>
+          <a href="#about">Tentang Kami</a>
+        </li>
+        <li>
+          <a href="#product">Produk</a>
+        </li>
+        <li>
+          <a href="#contact">Kontak</a>
+        </li>
+      </ul>
+      <Socmed socmedMedia={{ whatsapp: headerMedia.whatsapp, shopee: headerMedia.shopee, instagram: headerMedia.instagram }} />
     </header>
   );
 };
