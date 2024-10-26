1aebe2e5da4abc605b0f23fba2e066b01345b2dc
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-26 14:04:16 +0700

    Edit: use Logo component in header

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import "@/components/organism/header.style.css";
 import { HeaderMedia } from "@/types/components.types";
 import Socmed from "../molecules/Socmed";
+import Logo from "../atoms/Logo";
 
 interface HeaderProps {
   headerMedia: HeaderMedia;
@@ -10,9 +10,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ headerMedia }) => {
   return (
     <header id="home" className="header" style={{ backgroundImage: `url(${headerMedia.hero_image})` }}>
-      <div className="logo">
-        <Image src={headerMedia.logo_image} width={500} height={500} alt="Logo Dagingue" />
-      </div>
+      <Logo logoMedia={{ logo_image: headerMedia.logo_image }} />
       <ul className="navbar-header">
         <li>
           <a href="#about">Tentang Kami</a>
