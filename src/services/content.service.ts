740f10237aba75581f7693cffc876a57ab5c5fee
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-27 15:44:09 +0700

    Create: service for get data potong

--- src/services/content.service.ts
+++ src/services/content.service.ts
@@ -1,4 +1,4 @@
-import { AboutResponse, ApiResponse, MediaResponse } from "@/types/response.types";
+import { AboutResponse, ApiResponse, MediaResponse, PotongResponse } from "@/types/response.types";
 import axios from "axios";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -46,6 +46,41 @@ export const getAbout = async (): Promise<AboutResponse> => {
     const response = await axios.get<ApiResponse<AboutResponse[]>>(`${apiUrl}/content/about`);
     if (response.data.status) {
       return response.data.data[0];
+    } else {
+      throw new Error(
+        JSON.stringify({
+          status: response.data.status,
+          statusCode: response.data.statusCode,
+          message: response.data.message,
+        })
+      );
+    }
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        JSON.stringify({
+          status: error.response?.data?.status || false,
+          statusCode: error.response?.data?.statusCode || 500,
+          message: error.response?.data?.message || "Unexpected error occurred",
+        })
+      );
+    } else {
+      throw new Error(
+        JSON.stringify({
+          status: false,
+          statusCode: 500,
+          message: "Internal server error",
+        })
+      );
+    }
+  }
+};
+
+export const getPotong = async (): Promise<PotongResponse> => {
+  try {
+    const response = await axios.get<ApiResponse<PotongResponse>>(`${apiUrl}/product/potong`);
+    if (response.data.status) {
+      return response.data.data;
     } else {
       throw new Error(
         JSON.stringify({
