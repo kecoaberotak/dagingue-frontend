db55dfae714177b1e3f94e047296ceefe2b9389d
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-31 08:01:32 +0700

    Feat: implementing bumbu data context provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Header from "@/components/organism/Header";
 import About from "@/components/organism/About";
 import ProductPotong from "@/components/organism/ProductPotong";
 import ProductBumbu from "@/components/organism/ProductBumbu";
-import { getAbout, getMedia, getPotong } from "@/services/content.service";
-import { AboutResponse, MediaResponse, PotongResponse } from "@/types/response.types";
+import { getAbout, getBumbu, getMedia, getPotong } from "@/services/content.service";
+import { AboutResponse, BumbuResponse, MediaResponse, PotongResponse } from "@/types/response.types";
+import BumbuDataProvider from "@/contexts/BumbuDataContext";
 
 export default async function Homepage() {
   try {
@@ -21,13 +22,17 @@ export default async function Homepage() {
 
     const potongData: PotongResponse = await getPotong();
 
+    const bumbuData: BumbuResponse = await getBumbu();
+
     return (
       <div>
         <Header headerMedia={headerMedia} />
         <About aboutData={aboutData} />
         <div className="mb-[50px] grid gap-[50px]" id="product">
           <ProductPotong potongData={potongData} bg_image={mediaData.background_image} />
-          <ProductBumbu />
+          <BumbuDataProvider>
+            <ProductBumbu bumbuData={bumbuData} />
+          </BumbuDataProvider>
         </div>
       </div>
     );
