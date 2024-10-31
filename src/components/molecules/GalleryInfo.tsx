7754d2f1b481cca632a3ec1b3bd8d7b1e9a7d464
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-31 21:19:57 +0700

    Create: conditioning to check if BumbuData exist

--- src/components/molecules/GalleryInfo.tsx
+++ src/components/molecules/GalleryInfo.tsx
@@ -6,16 +6,15 @@ import Image from "next/image";
 const GalleryInfo = () => {
   const { bumbuData } = useBumbuData();
 
+  // Pastikan `bumbuData` memiliki setidaknya satu item sebelum merender konten
+  if (bumbuData.length === 0) return null;
+
   return (
     <section className="gallery-info">
-      <div className="gallery-info-image">{bumbuData.length > 0 && bumbuData[0].image && <Image src={bumbuData[0].image} alt={bumbuData[0].name} title={bumbuData[0].name} width={1080} height={1080} />}</div>
+      <div className="gallery-info-image">{bumbuData[0].image && <Image src={bumbuData[0].image} alt={bumbuData[0].name} title={bumbuData[0].name} width={1080} height={1080} />}</div>
       <div className="gallery-info-bumbu">
-        {bumbuData.length > 0 && bumbuData[0].desc && (
-          <>
-            <h2>{bumbuData[0].name}</h2>
-            <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: bumbuData[0].desc }}></div>
-          </>
-        )}
+        <h2>{bumbuData[0].name}</h2>
+        <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: bumbuData[0].desc }}></div>
       </div>
     </section>
   );
