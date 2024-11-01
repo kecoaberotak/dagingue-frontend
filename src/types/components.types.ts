bf2b9fec4afd7c8a4e3079c26b93b04526379082
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-11-01 20:26:47 +0700

    Create: type for SocmedMedia

--- src/types/components.types.ts
+++ src/types/components.types.ts
@@ -6,7 +6,21 @@ export interface HeaderMedia {
   shopee: string;
 }
 
+export interface FooterMedia {
+  footer_image: string;
+  logo_image: string;
+  background_image: string;
+  address: string;
+  email: string;
+  phone: number;
+  maps: string;
+  whatsapp: string;
+  shopee: string;
+  instagram: string;
+}
+
 export interface SocmedMedia {
+  className?: string;
   whatsapp: string;
   instagram: string;
   shopee: string;
