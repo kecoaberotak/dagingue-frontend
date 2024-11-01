c2693391a0076025b14817c0495e9aae693b133b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-11-01 20:25:48 +0700

    Edit: parameter

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
 
-interface logoProps {
-  logoMedia: { logo_image: string };
-}
-
-const Logo: React.FC<logoProps> = ({ logoMedia }) => {
+const Logo: React.FC<{ logo_image: string }> = ({ logo_image }) => {
   return (
     <div className="logo">
-      <Image src={logoMedia.logo_image} width={500} height={500} alt="Logo Dagingue" />
+      <Image src={logo_image} width={500} height={500} alt="Logo Dagingue" />
     </div>
   );
 };
