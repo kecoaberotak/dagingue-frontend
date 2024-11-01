94e8111751b3490b4322fb9d3225e570308f57c5
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-11-01 14:37:17 +0700

    Edit: used Image component

--- src/components/atoms/GalleryItemsImage.tsx
+++ src/components/atoms/GalleryItemsImage.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const GalleryItemsImage: React.FC<{ name: string; desc: string; image: string }> = ({ name, desc, image }) => {
   return (
     <section className="gallery-item-image">
-      <img src={image} alt={name} />
+      <Image src={image} alt={name} width={1080} height={1080} />
       <div className={`cover`} data-name={name} data-desc={desc} data-image={image} />
     </section>
   );
