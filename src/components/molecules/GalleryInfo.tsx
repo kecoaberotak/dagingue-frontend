257d69068027c6671b4bc8c08bcbd20378a5eb8d
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-11-01 18:31:38 +0700

    Feat: make conditioning based on context for SelectedImage

--- src/components/molecules/GalleryInfo.tsx
+++ src/components/molecules/GalleryInfo.tsx
@@ -1,20 +1,38 @@
 "use client";
 
 import { useBumbuData } from "@/hooks/useBumbuData";
+import { useSelectedImage } from "@/hooks/useSelectedImage ";
 import Image from "next/image";
 
 const GalleryInfo = () => {
   const { bumbuData } = useBumbuData();
+  const { selectedData } = useSelectedImage();
 
   // Pastikan `bumbuData` memiliki setidaknya satu item sebelum merender konten
   if (bumbuData.length === 0) return null;
 
+  if (!selectedData) {
+    return (
+      <section className="gallery-info">
+        <div className="gallery-info-image">
+          <Image src={bumbuData[0].image} alt={bumbuData[0].name} title={bumbuData[0].name} width={1080} height={1080} />
+        </div>
+        <div className="gallery-info-bumbu">
+          <h2>{bumbuData[0].name}</h2>
+          <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: bumbuData[0].desc }}></div>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className="gallery-info">
-      <div className="gallery-info-image">{bumbuData[0].image && <Image src={bumbuData[0].image} alt={bumbuData[0].name} title={bumbuData[0].name} width={1080} height={1080} />}</div>
+      <div className="gallery-info-image">
+        <Image src={selectedData.image} alt={selectedData.name} title={selectedData.name} width={1080} height={1080} />
+      </div>
       <div className="gallery-info-bumbu">
-        <h2>{bumbuData[0].name}</h2>
-        <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: bumbuData[0].desc }}></div>
+        <h2>{selectedData.name}</h2>
+        <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: selectedData.desc }}></div>
       </div>
     </section>
   );
