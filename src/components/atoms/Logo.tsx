0df705e4cb6e17a9060d8d91f5f7cff57d6bf50b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-11-04 16:03:54 +0700

    Add: attribute props className for component Logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-const Logo: React.FC<{ logo_image: string }> = ({ logo_image }) => {
+const Logo: React.FC<{ logo_image: string; className: string }> = ({ logo_image, className }) => {
   return (
-    <div className="logo">
+    <div className={className}>
       <Image src={logo_image} width={500} height={500} alt="Logo Dagingue" />
     </div>
   );
