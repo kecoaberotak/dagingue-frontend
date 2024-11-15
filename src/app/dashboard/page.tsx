d486e534f498711bcaa99f50959ac7dd999c316e
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-11-15 10:54:41 +0700

    Edit: make code more simple

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,45 +1,37 @@
 "use client";
 
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import AdminAbout from "@/components/organism/AdminAbout";
 
 // Definisikan komponen placeholder untuk setiap halaman
 const components = {
-  about: () => <AdminAbout />,
-  media: () => <h1>Media</h1>,
-  bumbu: () => <h1>Bumbu</h1>,
-  potong: () => <h1>Potong</h1>,
+  about: <AdminAbout />,
+  media: <h1>Media</h1>,
+  bumbu: <h1>Bumbu</h1>,
+  potong: <h1>Potong</h1>,
 };
-
-// Tipe untuk key yang valid di objek `components`
-type HalamanKey = keyof typeof components;
 
 const Dashboard = () => {
   // Atur tipe `halaman` agar sesuai dengan key yang valid di `components`
-  const [halaman, setHalaman] = useState<HalamanKey>("about");
-
-  // Dapatkan komponen sesuai halaman aktif atau default ke About
-  const ActiveComponent = components[halaman];
+  const [halaman, setHalaman] = useState<keyof typeof components>("about");
 
   return (
     <section className="admin-panel">
       <nav className="content-nav">
         <ul>
           {Object.keys(components).map((component) => (
-            <Fragment key={component}>
-              <li>
-                <a onClick={() => setHalaman(component as HalamanKey)}>{component.charAt(0).toUpperCase() + component.slice(1)}</a>
+            <>
+              <li key={component}>
+                <a onClick={() => setHalaman(component as keyof typeof components)}>{component.charAt(0).toUpperCase() + component.slice(1)}</a>
               </li>
               <li>
                 <span>/</span>
               </li>
-            </Fragment>
+            </>
           ))}
         </ul>
       </nav>
-      <main className="admin-panel-content">
-        <ActiveComponent />
-      </main>
+      <main className="admin-panel-content">{components[halaman]}</main>
     </section>
   );
 };
