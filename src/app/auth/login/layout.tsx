74ce26668b9c967ea0e051f287d72483ba97a32b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-12-13 10:55:18 +0700

    Create: layout for login page

---
+++ src/app/auth/login/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  variable: "--font-montserrat",
+});
+
+export const metadata: Metadata = {
+  title: "Login | Dagingue",
+  // content: "noindex",
+  // name: "robots",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} antialiased font-montserrat`}>{children}</body>
+    </html>
+  );
+}
